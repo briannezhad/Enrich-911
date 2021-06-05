4ce5bf116eda2badde331eff78acdb852073b980
Brian Nezhad <briannezhad@gmail.com>
2021-06-04 21:11:38 -0400

    Add Mapbox to App screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import React, { useRef, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import mapboxgl from 'mapbox-gl'
+
+// MapBox Token
+mapboxgl.accessToken = 'pk.eyJ1IjoidGVsb3ciLCJhIjoiY2tsbzJrMzloMDZocTMwcXlqandzdm5xYyJ9.UBxSH925tP1N50Q-3HLHtQ';
 
 function App() {
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/telow/cklocfpit5n8017p7o8p4nv7h',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
+
 
 export default App;
