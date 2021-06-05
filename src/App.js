956d5956e79de21096beec8156cd0ab30526e781
Brian Nezhad <briannezhad@gmail.com>
2021-06-04 23:42:21 -0400

    Update the Map, add Incident Markers

--- src/App.js
+++ src/App.js
@@ -1,56 +1,37 @@
 import React, { useRef, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import mapboxgl from 'mapbox-gl'
+import Map from "./components/Map/Map"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// MapBox Token
-mapboxgl.accessToken = 'pk.eyJ1IjoidGVsb3ciLCJhIjoiY2tsbzJrMzloMDZocTMwcXlqandzdm5xYyJ9.UBxSH925tP1N50Q-3HLHtQ';
-
+// Incident Files Example (It would be cool if this was iterating from API call)
+var incident_one = require('./data/F01705150050.json');
+var incident_two = require('./data/F01705150090.json');
 
 function App() {
-
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/telow/cklocfpit5n8017p7o8p4nv7h',
-      center: [lng, lat],
-      zoom: zoom
-    });
-  });
-
   return (
     <div className="App">
-      <div ref={mapContainer} className="map-container" />
-      <div className="incidents-container">
-      <table class="table">
+      <Map />
+
+      <div className="incidents-container p-3 shadow-lg">
+      <table class="table m-0">
         <thead>
           <tr>
             <th scope="col">Incident #</th>
             <th scope="col">Address</th>
             <th scope="col">Unit Type</th>
-            <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td><button class="btn btn-danger" type="submit">Detail</button></td>
+            <th scope="row">{incident_one.description.incident_number}</th>
+            <td>{incident_one.address.address_line1}</td>
+            <td>{incident_one.apparatus[0].unit_type}</td>
           </tr>
           <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td><button class="btn btn-danger" type="submit">Detail</button></td>
+            <th scope="row">{incident_two.description.incident_number}</th>
+            <td>{incident_two.address.address_line1}</td>
+            <td>{incident_two.apparatus[0].unit_type}</td>
           </tr>
         </tbody>
       </table>
